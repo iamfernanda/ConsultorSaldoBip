44076f1cec4ab04788e532010f8e14cb2c4058a4
iamfernanda <fda.isla@gmail.com>
2018-07-03 00:56:38 -0400

    html 2 y más

---
+++ js/btns.js
@@ -0,0 +1,4 @@
+// Boton perfil //
+function uno() {
+ document.getElementById("boxPerfil").style.visibility = "visible";
+}
--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
-// function busqueda() {
-// document.getElementById("box2").style.visibility = "visible";
-//  }
-
 //llamado a la api con fetch//
 fetch("http://www.psep.cl/api/Bip.php?&numberBip=666"
 ).then(response => response.json()
 ).then(respuestaJason => {
-    console.log(respuestaJason);
+    // console.log(respuestaJason);
 })
